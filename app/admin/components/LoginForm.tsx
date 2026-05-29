023da8d5285defc35253eae9b4c666691729a89d
OzkrMebasser <oscarmorenoestudios@gmail.com>
2026-05-29 02:24:24 -0700

    fixing url to login admi

--- app/admin/components/LoginForm.tsx
+++ app/admin/components/LoginForm.tsx
@@ -18,7 +18,7 @@ export default function LoginForm() {
     const { error } = await supabase.auth.signInWithOAuth({
       provider: 'google',
       options: {
-        redirectTo: `${window.location.origin}/admin`,
+       redirectTo: `${process.env.NEXT_PUBLIC_SITE_URL || window.location.origin}/admin`,
       },
     });
 
